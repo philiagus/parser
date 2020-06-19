06bde11a93192dfac0b52e5c407cbadfeacbd379
Andreas Bittner <philiagus@philiagus.de>
2020-06-19 15:56:23 +0200

    #10 & #12 Add Fail and Any parsers (#15)

---
+++ tests/Philiagus/Parser/Test/Unit/Parser/FailTest.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * This file is part of philiagus/parser
+ *
+ * (c) Andreas Bittner <philiagus@philiagus.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Philiagus\Parser\Test\Unit\Parser;
+
+use Philiagus\Parser\Base\Parser;
+use Philiagus\Parser\Exception\ParsingException;
+use Philiagus\Parser\Parser\Fail;
+use Philiagus\Parser\Test\Provider\DataProvider;
+use PHPUnit\Framework\TestCase;
+
+class FailTest extends TestCase
+{
+    public function testThatItExtendsBaseParser(): void
+    {
+        self::assertTrue((new Fail()) instanceof Parser);
+    }
+
+    public function provideAnyValue(): array
+    {
+        return DataProvider::provide(DataProvider::TYPE_ALL);
+    }
+
+    /**
+     * @param $value
+     *
+     * @throws \Philiagus\Parser\Exception\ParserConfigurationException
+     * @throws \Philiagus\Parser\Exception\ParsingException
+     * @dataProvider provideAnyValue
+     */
+    public function testItFailsOnAnyValue($value): void
+    {
+        $this->expectException(ParsingException::class);
+        $this->expectExceptionMessage('This value can never match');
+        Fail::new()->parse($value);
+    }
+
+    public function testMessageOverwriteAndReplacers(): void
+    {
+        $this->expectException(ParsingException::class);
+        $this->expectExceptionMessage('1 integer integer 1');
+        Fail::message('{value} {value.type} {value.debug}')->parse(1);
+    }
+
+    public function testStaticMessageconstructor(): void
+    {
+        $this->expectException(ParsingException::class);
+        $this->expectExceptionMessage('1 integer integer 1');
+        (new Fail())
+            ->overwriteExceptionMessage('{value} {value.type} {value.debug}')
+            ->parse(1);
+    }
+
+}
